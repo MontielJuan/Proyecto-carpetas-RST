622ac8f6764b9250f23b705039055bdc9c13db58
Jhon Serna Durango <jhon.serna@sofka.com.co>
2022-02-14 19:38:38 -0500

    Comunicando carpetas y firebase

--- src/pages/almacen.jsx
+++ src/pages/almacen.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import Header from "../components/Header";
 import Tree from "../components/Tree";
+import firebase from "../firebase";
 
 const treeData = [
   {
@@ -57,19 +59,63 @@ const treeData = [
 ];
 
 const TreeList = () => {
+  useEffect(() => {
+    // const dataRef = firebase.storage().ref("Raspi montiel/Mes/Tuesday").listAll().then(function(result) {
+    //   result.items.forEach(ref => {
+    //     ref.getMetadata().then(url => {
+    //       console.log(url)
+    //     }).catch(e => {
+    //       console.log(e);
+    //     })
+    //   })
+    // });
+
+    var firstPaths = [];
+
+    firebase
+      .storage()
+      .ref("Raspi montiel")
+      .listAll()
+      .then((result) => {
+        result.prefixes.forEach((mes) => {
+          firstPaths.push(mes.fullPath);
+          console.log(mes.name);
+          firebase
+            .storage()
+            .ref(mes.fullPath)
+            .listAll()
+            .then((result2) => {
+              result2.prefixes.forEach((dia) => {
+                console.log(dia.name);
+                firebase
+                  .storage()
+                  .ref(dia.fullPath)
+                  .listAll()
+                  .then((result3) => {
+                    result3.items.forEach((archivo) => {
+                      console.log(dia.name);
+                      console.log(archivo.name);
+                    });
+                  });
+              });
+            });
+        });
+      });
+  }, []);
+
   return (
     <>
-	<Header title="Almacen"></Header>
+      <Header title="Almacen"></Header>
       <div className="row">
         <div className="col text-center">
           <h2>Almacen</h2>
-          <p className="mt-3">
+          <div className="mt-3">
             <div className="row mt-3 d-flex justify-content-center">
               <div className="col-lg-8 text-left text-dark">
                 <Tree data={treeData} />
               </div>
             </div>
-          </p>
+          </div>
         </div>
       </div>
     </>
